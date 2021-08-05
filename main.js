75448a327435eec0d2d1ef6cde0ad095ccb443d7
gerd86 <15376064+gerd86@users.noreply.github.com>
2021-08-05 18:15:33 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const fetch = require("node-fetch");
+
+fetch("https://type.fit/api/quotes")
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    console.log(data);
+  });
